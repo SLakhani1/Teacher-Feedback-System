2d559966ba0fc99c532f67b83445647d5f98c99f
Ashutosh-Kaushik <icm2017002@iiita.ac.in>
2019-04-19 16:33:21 +0530

    models_updated

--- SabKuch/admin.py
+++ SabKuch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Student)
+admin.site.register(models.Teacher)
+admin.site.register(models.Feedback)
+admin.site.register(models.Course)
