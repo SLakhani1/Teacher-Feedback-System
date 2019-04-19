b8cf4030aad61a1447ab0f873faff93d95edfa65
SLakhani1 <sagar.lakhani63@gmail.com>
2019-04-19 17:06:26 +0530

    Feedback Form

--- SabKuch/views.py
+++ SabKuch/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
-
+def create_feedback(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.save()
+            return redirect(pass)
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback_create.html', {'form': form})
