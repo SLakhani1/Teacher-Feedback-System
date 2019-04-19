0886cccab04ca59ea05bbcc2a53107f7472a15b9
Ashutosh-Kaushik <icm2017002@iiita.ac.in>
2019-04-19 16:30:29 +0530

    models_updated

--- SabKuch/models.py
+++ SabKuch/models.py
@@ -1,46 +1,48 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 from django.db.models import Max, Min, Avg
+from django.contrib.auth.models import User
 
+class Course(models.Model):
+    course_code = models.CharField(max_length=20, primary_key=True, blank = False)
+    course_name = models.CharField(max_length=40, blank = False)
+    session = models.CharField(max_length=20, blank = False)
+    ltp = models.PositiveIntegerField(validators = [MaxValueValidator(333)], blank = False)
+    
+    def __str__(self):
+        return self.course_code
 
 class Teacher(models.Model):
-    fac_code = models.CharField(primary_key=True,max_length=5)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    designation = models.CharField(max_length=20)
-    department = models.CharField(max_length=10)
+    user = models.OneToOneField(User)
+    fac_code = models.CharField(primary_key=True,max_length=5, blank = False)
+    first_name = models.CharField(max_length=20, blank = False)
+    last_name = models.CharField(max_length=20, blank = False)
+    designation = models.CharField(max_length=20 , blank = False)
+    department = models.CharField(max_length=10, blank = False)
+    courses = models.ManyToManyField(Course, blank = False, related_name='course_teacher')
 
     def __str__(self):
         return self.fac_code
 
 class Student(models.Model):
+    user = models.OneToOneField(User)
     enrollment_id = models.CharField(max_length=20, primary_key=True)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    degree = models.CharField(max_length=20)
-    joining_year = models.DateField()
+    first_name = models.CharField(max_length=20, blank = False)
+    last_name = models.CharField(max_length=20, blank = False)
+    semester = models.PositiveIntegerField(validators = [MaxValueValidator(10)], blank = False)
 
     def __str__(self):
         return self.enrollment_id
-    
-class Course(models.Model):
-    course_code = models.CharField(max_length=20, primary_key=True)
-    course_name = models.CharField(max_length=40)
-    session = models.CharField(max_length=20)
-    ltp = models.PositiveIntegerField(validators = [MaxValueValidator(333)])
-    
-    def __str__(self):
-        return self.course_code
 
 class Feedback(models.Model):
-    feedback_id = models.AutoField(primary_key=True)
-    teacher_id = models.ForeignKey('teachers.Teacher', on_delete=models.CASCADE, related_name='fac_codes')
-    course_id = models.ForeignKey('teachers.Course', on_delete=models.CASCADE, related_name='course_codes')
-    field1 = models.PositiveIntegerField(validators = [MaxValueValidator(5)])
-    field2 = models.PositiveIntegerField(validators = [MaxValueValidator(5)])
-    field3 = models.PositiveIntegerField(validators = [MaxValueValidator(5)])
-    comment1 = models.TextField()
-    comment2 = models.TextField()
+    student_id = models.ForeignKey(Student,on_delete=models.CASCADE, blank = False)
+    feedback_id = models.AutoField(primary_key=True,blank = False)
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='fac_codes', blank = False)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_codes', blank = False)
+    skills = models.PositiveIntegerField(validators = [MaxValueValidator(5)], default = 1)
+    knowledge  = models.PositiveIntegerField(validators = [MaxValueValidator(5)], default = 1)
+    interactivity = models.PositiveIntegerField(validators = [MaxValueValidator(5)], default = 1)
+    review = models.TextField(blank = False)
 
     def __str__(self):
         return str(self.feedback_id)
