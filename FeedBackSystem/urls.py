5193875c7e365ef7333f6836246882e5b31f0a85
SLakhani1 <sagar.lakhani63@gmail.com>
2019-04-19 17:17:44 +0530

    Form added

--- FeedBackSystem/urls.py
+++ FeedBackSystem/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from SabKuch import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.HomePage)
+    url(r'^$',views.HomePage),
+    url(r'^create/feedback/$',views.create_feedback),
 ]
--- SabKuch/views.py
+++ SabKuch/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import FeedbackForm
 
+def HomePage(request):
+    return render(request,'login.html')
+
 def create_feedback(request):
     if request.method == "POST":
         form = FeedbackForm(request.POST)
         if form.is_valid():
             feedback = form.save(commit=False)
             feedback.save()
-            return redirect(pass)
+            return redirect('login.html')
     else:
         form = FeedbackForm()
     return render(request, 'feedback_create.html', {'form': form})
 from django.shortcuts import render
-
-# Create your views here.
-
-def HomePage(request):
-    return render(request,'login.html')
