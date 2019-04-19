9b39b81923e9bf9a5d47dddf59c6636eb146222c
SLakhani1 <sagar.lakhani63@gmail.com>
2019-04-19 16:44:58 +0530

    Tables

--- SabKuch/admin.py
+++ SabKuch/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
+
 admin.site.register(models.Student)
 admin.site.register(models.Teacher)
 admin.site.register(models.Feedback)
